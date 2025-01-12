103c7fd85776099f82cc7295ee50be81f850a2e9
Dhruv Khurana <dhruv.khurana@tts.edu.sg>
2025-01-12 17:20:39 +0800

    Add files via upload

---
+++ SevenSegmentDisplay.cpp
@@ -0,0 +1,82 @@
+#include "SevenSegmentDisplay.h"
+
+SevenSegmentDisplay::SevenSegmentDisplay(int Dot, int TopRight, int BottomRight, int Top, int Bottom, int TopLeft, int BottomLeft, int Middle)
+  : dot(Dot), topRight(TopRight), bottomRight(BottomRight), top(Top), bottom(Bottom), topLeft(TopLeft), bottomLeft(BottomLeft), middle(Middle) {}
+
+void SevenSegmentDisplay::start() {
+  pinMode(top, OUTPUT);
+  pinMode(topLeft, OUTPUT);
+  pinMode(topRight, OUTPUT);
+  pinMode(middle, OUTPUT);
+  pinMode(bottom, OUTPUT);
+  pinMode(bottomLeft, OUTPUT);
+  pinMode(bottomRight, OUTPUT);
+  pinMode(dot, OUTPUT);
+}
+
+void SevenSegmentDisplay::clear() {
+  digitalWrite(top, LOW);
+  digitalWrite(middle,LOW);
+  digitalWrite(bottom, LOW);
+  digitalWrite(topLeft, LOW);
+  digitalWrite(bottomRight, LOW);
+  digitalWrite(bottomLeft, LOW);
+  digitalWrite(topRight, LOW);
+}
+
+void SevenSegmentDisplay::display(int num) {
+  clear();
+  if (num == 0) {
+    Zero();
+  } else if (num == 1) {
+    One();      
+  } else if (num == 2) {
+    Two();
+  } else if (num == 3) {
+    Three();
+  } else if (num == 4) {
+      Four();
+  } else if (num == 5) {
+    Five();
+  } else if (num == 6) {
+    Six();
+  } else if (num == 7) {
+    Seven();
+  } else if (num == 8) {
+    Eight();
+  } else if (num == 9) {
+    Nine();
+  } else {
+    clear();
+  }
+}
+
+void SevenSegmentDisplay::test(int pause = 1000) {
+  One();
+  delay(pause);
+  Two();
+  delay(pause);
+  Three();
+  delay(pause);
+  Four();
+  delay(pause);
+  Five();
+  delay(pause);
+  Six();
+  delay(pause);
+  Seven();
+  delay(pause);
+  Eight();
+  delay(pause);
+  Nine();
+  delay(pause);
+  Zero();
+  delay(pause);
+}
+
+void SevenSegmentDisplay::potDemo(int potPin) {
+  val = analogRead(potPin);
+  delay(2);
+  num = map(val, 0, 1023, 0, 9);
+  display(num);
+}
---
+++ SevenSegmentDisplay.h
@@ -0,0 +1,185 @@
+#ifndef SEVENSEGMENTDISPLAY_H
+#define SEVENSEGMENTDISPLAY_H
+
+#include <Arduino.h>
+
+class SevenSegmentDisplay {
+  private:
+    const int dot;
+    const int topRight;
+    const int bottomRight;
+    const int top;
+    const int bottom;
+    const int topLeft;
+    const int bottomLeft;
+    const int middle;
+    const int pot;
+    int val;
+    int num;
+
+    void One() {
+      digitalWrite(top, LOW);
+      digitalWrite(middle,LOW);
+      digitalWrite(bottom, LOW);
+      digitalWrite(topLeft, LOW);
+      digitalWrite(bottomRight, LOW);
+      digitalWrite(bottomLeft, LOW);
+      digitalWrite(topRight, LOW);
+      digitalWrite(topRight, HIGH);
+      digitalWrite(bottomRight, HIGH);
+    }
+
+    void Two() {
+      digitalWrite(top, LOW);
+      digitalWrite(middle,LOW);
+      digitalWrite(bottom, LOW);
+      digitalWrite(topLeft, LOW);
+      digitalWrite(bottomRight, LOW);
+      digitalWrite(bottomLeft, LOW);
+      digitalWrite(topRight, LOW);
+      digitalWrite(top, HIGH);
+      digitalWrite(middle,HIGH);
+      digitalWrite(bottom, HIGH);
+      digitalWrite(topRight, HIGH);
+      digitalWrite(bottomLeft, HIGH);
+    }
+
+    void Three() {
+      digitalWrite(top, LOW);
+      digitalWrite(middle,LOW);
+      digitalWrite(bottom, LOW);
+      digitalWrite(topLeft, LOW);
+      digitalWrite(bottomRight, LOW);
+      digitalWrite(bottomLeft, LOW);
+      digitalWrite(topRight, LOW);
+      digitalWrite(top, HIGH);
+      digitalWrite(middle,HIGH);
+      digitalWrite(bottom, HIGH);
+      digitalWrite(topRight, HIGH);
+      digitalWrite(bottomRight, HIGH);
+    }
+
+    void Four() {
+      digitalWrite(top, LOW);
+      digitalWrite(middle,LOW);
+      digitalWrite(bottom, LOW);
+      digitalWrite(topLeft, LOW);
+      digitalWrite(bottomRight, LOW);
+      digitalWrite(bottomLeft, LOW);
+      digitalWrite(topRight, LOW);
+      digitalWrite(topLeft, HIGH);
+      digitalWrite(middle,HIGH);
+      digitalWrite(topRight, HIGH);
+      digitalWrite(bottomRight, HIGH);
+    }
+
+    void Five() {
+      digitalWrite(top, LOW);
+      digitalWrite(middle,LOW);
+      digitalWrite(bottom, LOW);
+      digitalWrite(topLeft, LOW);
+      digitalWrite(bottomRight, LOW);
+      digitalWrite(bottomLeft, LOW);
+      digitalWrite(topRight, LOW);
+      digitalWrite(top, HIGH);
+      digitalWrite(middle,HIGH);
+      digitalWrite(bottom, HIGH);
+      digitalWrite(topLeft, HIGH);
+      digitalWrite(bottomRight, HIGH);
+    }
+
+    void Six() {
+      digitalWrite(top, LOW);
+      digitalWrite(middle,LOW);
+      digitalWrite(bottom, LOW);
+      digitalWrite(topLeft, LOW);
+      digitalWrite(bottomRight, LOW);
+      digitalWrite(bottomLeft, LOW);
+      digitalWrite(topRight, LOW);
+      digitalWrite(top, HIGH);
+      digitalWrite(middle,HIGH);
+      digitalWrite(bottom, HIGH);
+      digitalWrite(topLeft, HIGH);
+      digitalWrite(bottomRight, HIGH);
+      digitalWrite(bottomLeft, HIGH);
+
+    }
+
+    void Seven() {
+      digitalWrite(top, LOW);
+      digitalWrite(middle,LOW);
+      digitalWrite(bottom, LOW);
+      digitalWrite(topLeft, LOW);
+      digitalWrite(bottomRight, LOW);
+      digitalWrite(bottomLeft, LOW);
+      digitalWrite(topRight, LOW);
+      digitalWrite(top, HIGH);
+      digitalWrite(topRight,HIGH);
+      digitalWrite(bottomRight, HIGH);
+    }
+
+    void Eight() {
+      digitalWrite(top, LOW);
+      digitalWrite(middle,LOW);
+      digitalWrite(bottom, LOW);
+      digitalWrite(topLeft, LOW);
+      digitalWrite(bottomRight, LOW);
+      digitalWrite(bottomLeft, LOW);
+      digitalWrite(topRight, LOW);
+      digitalWrite(top, HIGH);
+      digitalWrite(middle,HIGH);
+      digitalWrite(bottom, HIGH);
+      digitalWrite(topLeft, HIGH);
+      digitalWrite(bottomRight, HIGH);
+      digitalWrite(bottomLeft, HIGH);
+      digitalWrite(topRight, HIGH);
+    }
+
+    void Nine() {
+      digitalWrite(top, LOW);
+      digitalWrite(middle,LOW);
+      digitalWrite(bottom, LOW);
+      digitalWrite(topLeft, LOW);
+      digitalWrite(bottomRight, LOW);
+      digitalWrite(bottomLeft, LOW);
+      digitalWrite(topRight, LOW);
+      digitalWrite(top, HIGH);
+      digitalWrite(middle,HIGH);
+      digitalWrite(bottomRight, HIGH);
+      digitalWrite(topLeft, HIGH);
+      digitalWrite(topRight, HIGH);
+      digitalWrite(bottom, HIGH);
+    }
+
+    void Zero() {
+      digitalWrite(top, LOW);
+      digitalWrite(middle,LOW);
+      digitalWrite(bottom, LOW);
+      digitalWrite(topLeft, LOW);
+      digitalWrite(bottomRight, LOW);
+      digitalWrite(bottomLeft, LOW);
+      digitalWrite(topRight, LOW);
+      digitalWrite(top, HIGH);
+      digitalWrite(bottom, HIGH);
+      digitalWrite(topLeft, HIGH);
+      digitalWrite(bottomRight, HIGH);
+      digitalWrite(bottomLeft, HIGH);
+      digitalWrite(topRight, HIGH);
+    }
+
+  public:
+    SevenSegmentDisplay() {}
+    SevenSegmentDisplay(int Dot, int TopRight, int BottomRight, int Top, int Bottom, int TopLeft, int BottomLeft, int Middle);
+
+    void start();
+
+    void clear();
+
+    void display(int num);
+
+    void test(int pause = 1000);
+
+    void potDemo(int potPin);
+};
+
+#endif
